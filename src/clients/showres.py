8c7c0e2bffa1104298e129dd578e5da6e8f2c0e3
Narayan Desai <desai@mcs.anl.gov>
2006-03-15 16:31:37 +0000

    Added showres

---
+++ src/clients/showres.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+'''Display reservations'''
+__revision__ = '$Revision$'
+
+import time
+import Cobalt.Proxy, Cobalt.Logging, Cobalt.Util
+
+if __name__ == '__main__':
+    Cobalt.Logging.setup_logging('showres', to_syslog=False)
+    scheduler = Cobalt.Proxy.scheduler()
+    reservations = {}
+    for partition in scheduler.GetPartitions([{'tag':'partition', 'name':'*', 'reservations':'*'}]):
+        for reservation in partition['reservations']:
+            if reservations.has_key(reservation):
+                reservations[reservation].append(partition['name'])
+            else:
+                reservations[reservation] = [partition['name']]
+
+    output = [('Reservation', 'User', 'Start', 'Duration', 'Partitions')]
+    for ((name, user, start, duration), partitions) in reservations.iteritems():
+        output.append((name, user, time.strftime("%c", time.localtime(start)), duration, str(partitions)))
+    Cobalt.Util.print_tabular(output)
+                     
