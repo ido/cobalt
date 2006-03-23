2553f245aba569d3ce8d8bfc30d62562d35aea76
Theron Voran <voran@mcs.anl.gov>
2006-03-23 04:56:05 +0000

    made output like cqstat: Name, Queue, State

--- src/clients/partlist.py
+++ src/clients/partlist.py
@@ -3,7 +3,7 @@
 '''Partlist displays online partitions for users'''
 __revision__ = '$Revision$'
 
-import Cobalt.Proxy
+import Cobalt.Proxy, Cobalt.Util
 
 helpmsg = '''Usage: partlist'''
 
@@ -11,5 +11,8 @@ if __name__ == '__main__':
     sched = Cobalt.Proxy.scheduler()
     parts = sched.GetPartition([{'tag':'partition', 'name':'*', 'queue':'*', 'state':'*', \
                                  'scheduled':True, 'functional':True}])
-    for part in parts:
-        print part
+
+    header = [['Name', 'Queue', 'State']]
+
+    output = [[part.get(x) for x in [y.lower() for y in header[0]]] for part in parts]
+    Cobalt.Util.print_tabular([tuple(x) for x in header + output])
