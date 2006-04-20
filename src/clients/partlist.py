1a8ac32df869966b8da50873d7253bf0024c96ea
Narayan Desai <desai@mcs.anl.gov>
2006-04-20 01:56:49 +0000

    Fix partlist dependency checking for cases where intermediate partitions are not scheduled

--- src/clients/partlist.py
+++ src/clients/partlist.py
@@ -10,7 +10,7 @@ helpmsg = '''Usage: partlist'''
 if __name__ == '__main__':
     sched = Cobalt.Proxy.scheduler()
     parts = sched.GetPartition([{'tag':'partition', 'name':'*', 'queue':'*', 'state':'*', \
-                                 'scheduled':True, 'functional':'*', 'deps':'*'}])
+                                 'scheduled':'*', 'functional':'*', 'deps':'*'}])
     partinfo = Cobalt.Util.buildRackTopology(parts)
     # need to cascade up busy
     busy = [part['name'] for part in parts if part['state'] == 'busy']
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # need to cascade up non-functional
     offline = [part['name'] for part in parts if not part['functional']]
     [part.__setitem__('functional', False) for part in parts for pname in offline if pname in part['deps']]
-    online = [part for part in parts if part['functional']]
+    online = [part for part in parts if part['functional'] and part['scheduled']]
     header = [['Name', 'Queue', 'State']]
     output = [[part.get(x) for x in [y.lower() for y in header[0]]] for part in online]
     Cobalt.Util.printTabular(header + output)
