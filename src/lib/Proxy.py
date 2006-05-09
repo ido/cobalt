c4249cb6c6ee02d2578780ca01cefa11783591b4
Narayan Desai <desai@mcs.anl.gov>
2006-05-09 15:58:01 +0000

    Add Drain/Resume support

--- src/lib/Proxy.py
+++ src/lib/Proxy.py
@@ -120,7 +120,7 @@ class process_manager(ComponentProxy):
 class queue_manager(ComponentProxy):
     '''queue manager proxy'''
     name = 'queue-manager'
-    methods = ['AddJob', 'GetJobs', 'DelJobs', 'RunJobs', 'SetJobs', 'SetJobID']
+    methods = ['AddJob', 'GetJobs', 'DelJobs', 'RunJobs', 'SetJobs', 'SetJobID', 'Drain', 'Resume']
 
 class scheduler(ComponentProxy):
     '''scheduler proxy'''
