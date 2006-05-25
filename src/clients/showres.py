2a4f90c8744f8467396d983aae2aae9e2c11410e
Narayan Desai <desai@mcs.anl.gov>
2006-05-25 18:48:06 +0000

    * Collapse and star completed reserved partitions

--- src/clients/showres.py
+++ src/clients/showres.py
@@ -9,7 +9,11 @@ if __name__ == '__main__':
     Cobalt.Logging.setup_logging('showres', to_syslog=False, level=20)
     scheduler = Cobalt.Proxy.scheduler()
     reservations = {}
-    for partition in scheduler.GetPartition([{'tag':'partition', 'name':'*', 'reservations':'*'}]):
+    partitions = scheduler.GetPartition([{'size':'*', 'tag':'partition', 'name':'*', 'reservations':'*', 'deps':'*'}])
+    npart = {}
+    [npart.__setitem__(partition.get('name'), partition) for partition in partitions]
+    depinfo = Cobalt.Util.buildRackTopology(partitions)
+    for partition in partitions:
         for reservation in partition['reservations']:
             if reservations.has_key(tuple(reservation)):
                 reservations[tuple(reservation)].append(partition['name'])
@@ -27,6 +31,10 @@ if __name__ == '__main__':
     else:
         output = [('Reservation', 'User', 'Start', 'Duration', 'Partitions')]
         for ((name, user, start, duration), partitions) in reservations.iteritems():
+            maxsize = max([npart[part].get('size') for part in partitions])
+            toppart = [npart[part] for part in partitions if npart[part].get('size') == maxsize][0].get('name')
+            if len([part for part in partitions if part in depinfo[toppart][1]]) == len(depinfo[toppart][1]):
+                partitions = toppart + '*'
             dmin = duration/60.0
             dhour = dmin/60
             dmin = dmin - (dhour * 60)
