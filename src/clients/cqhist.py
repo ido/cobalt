3a384747144e671deb0488ca0666685165053404
Theron Voran <voran@mcs.anl.gov>
2006-06-19 19:34:52 +0000

    * Added a '-d days' option, for specifying how many days back to show history for.    (Note this is not the number of 24-hour periods. For instance, -d 1 looks at jobs from midnight until now.)

--- src/clients/cqhist.py
+++ src/clients/cqhist.py
@@ -1,71 +1,77 @@
 #!/usr/bin/env python
 
-import ConfigParser, glob, re, math, datetime, sys
+import ConfigParser, glob, re, math, datetime, sys, time, getopt
 import Cobalt.Util
 
-# get current day
-current_day = datetime.date.today().strftime("%Y-%m-%d")
 
-user_re = "(?P<user>\w+)"
-jobid_re = "(?P<jobid>\d+)"
-location_re = "(?P<location>\S+)"
+if __name__ == '__main__':
+    (opts, command) = Cobalt.Util.dgetopt(sys.argv[1:], {'f':'full'}, {'d':'days'},
+                                          "Usage: cqhist [-d days] [-f]")
+    if not opts['days']:
+        opts['days'] = 1
+    # get day range
+    day_range = [time.strftime("%Y-%m-%d", time.localtime(int(time.time() - d*24*3600))) for d in range(int(opts['days']))]
+    day_range = '(' + '|'.join(day_range) + ')'
 
-done_re = re.compile("(?P<time>" + current_day + " \d+:\d+:\d+),\d{,3};Job " + \
-                     jobid_re + "/" + user_re + " on (?P<nodes>\d+) nodes done. queue:(?P<queuetime>\d+\.\d+)s user:(?P<usertime>\d+\.\d+)s*")
-start_re = re.compile("(?P<time>" + current_day + " \d+:\d+:\d+),\d{,3};S;(?P<jobid>\d+);" + \
-                      user_re + ";" + location_re + ";(\d+);(?P<nodes>\d+);(?P<processors>\d+);(?P<mode>\w+);(?P<walltime>\d+)")
-run_re = re.compile("(?P<time>" + current_day + " \d+:\d+:\d+),\d{,3};Job " + \
-                    jobid_re + "/" + user_re + "/Q:" + "(?P<queue>\w+): Running job on " + location_re)
+    # setup regex's
+    user_re = "(?P<user>\w+)"
+    jobid_re = "(?P<jobid>\d+)"
+    location_re = "(?P<location>\S+)"
 
-_config = ConfigParser.ConfigParser()
+    done_re = re.compile("(?P<time>" + day_range + " \d+:\d+:\d+),\d{,3};Job " + \
+                         jobid_re + "/" + user_re + " on (?P<nodes>\d+) nodes done. queue:(?P<queuetime>\d+\.\d+)s user:(?P<usertime>\d+\.\d+)s*")
+    start_re = re.compile("(?P<time>" + day_range + " \d+:\d+:\d+),\d{,3};S;(?P<jobid>\d+);" + \
+                          user_re + ";" + location_re + ";(\d+);(?P<nodes>\d+);(?P<processors>\d+);(?P<mode>\w+);(?P<walltime>\d+)")
+    run_re = re.compile("(?P<time>" + day_range + " \d+:\d+:\d+),\d{,3};Job " + \
+                        jobid_re + "/" + user_re + "/Q:" + "(?P<queue>\w+): Running job on " + location_re)
 
-_config.read('/etc/cobalt.conf')
+    _config = ConfigParser.ConfigParser()
+    _config.read('/etc/cobalt.conf')
+    cqm_section = _config._sections['cqm']
 
-cqm_section = _config._sections['cqm']
+    logfiles = glob.glob(cqm_section['log_dir'] + '/cqm*.log')
+    logfiles.sort()
+    #logfiles.reverse()  # put most recent log first
 
-logfiles = glob.glob(cqm_section['log_dir'] + '/cqm*.log')
+    rundict = {}
+    donedict = {}
+    startdict = {}
 
-logfiles.sort()
-logfiles.reverse()  # put most recent log first
+    output = []
 
-rundict = {}
-donedict = {}
-startdict = {}
+    for logfile in logfiles:
+        thefile = open(logfile, 'r')
+        for line in thefile:
+            d = done_re.match(line)
+            if d:
+                # format usertime
+                minutes, seconds = divmod(float(d.group('usertime')), 60)
+                hours, minutes = divmod(minutes, 60)
+                usertime = "%02d:%02d:%02d" % (hours, minutes, seconds)
 
-output = []
+                # format queuetime
+                minutes, seconds = divmod(float(d.group('queuetime')), 60)
+                hours, minutes = divmod(minutes, 60)
+                queuetime = "%02d:%02d:%02d" % (hours, minutes, seconds)
+                output.append([d.group('time'), d.group('jobid'), d.group('user'),
+                               d.group('nodes'), queuetime, usertime])
 
-thefile = open(logfiles[0], 'r')
-for line in thefile:
-    d = done_re.match(line)
-    if d:
-        # format usertime
-        minutes, seconds = divmod(float(d.group('usertime')), 60)
-        hours, minutes = divmod(minutes, 60)
-        usertime = "%02d:%02d:%02d" % (hours, minutes, seconds)
+            r = run_re.match(line)
+            if r:
+                rundict.update({r.group('jobid'):{ 'queue':r.group('queue'),
+                                                   'location':r.group('location') } })
 
-        # format queuetime
-        minutes, seconds = divmod(float(d.group('queuetime')), 60)
-        hours, minutes = divmod(minutes, 60)
-        queuetime = "%02d:%02d:%02d" % (hours, minutes, seconds)
-        output.append([d.group('time'), d.group('jobid'), d.group('user'),
-                       d.group('nodes'), queuetime, usertime])
+            s = start_re.match(line)
+            if s:
+                startdict.update({s.group('jobid'):{ 'processors':s.group('processors'),
+                                                     'nodes':s.group('nodes'), 'mode':s.group('mode'),
+                                                     'walltime':s.group('walltime') } })
 
-    r = run_re.match(line)
-    if r:
-        rundict.update({r.group('jobid'):{ 'queue':r.group('queue'),
-                                           'location':r.group('location') } })
-        
-    s = start_re.match(line)
-    if s:
-        startdict.update({s.group('jobid'):{ 'processors':s.group('processors'),
-                                             'nodes':s.group('nodes'), 'mode':s.group('mode'),
-                                             'walltime':s.group('walltime') } })
+        thefile.close()
 
-thefile.close()
+    uniqified = []
 
-uniqified = []
-
-for line in range( len(output) ):
+    for line in range( len(output) ):
 
         jobid = output[line][1]
 
@@ -78,18 +84,18 @@ for line in range( len(output) ):
         if '-f' in sys.argv:
             if startdict.has_key(jobid):
                 output[line] = output[line] + [ startdict[jobid]['processors'],
-                                                startdict[jobid]['mode'] ]
+                                            startdict[jobid]['mode'] ]
             else:
                 output[line] = output[line] + ['-', '-']
 
-for line in output:
-    if uniqified.count(line) == 0:
-        uniqified.append(line)
+    for line in output:
+        if uniqified.count(line) == 0:
+            uniqified.append(line)
 
-if '-f' in sys.argv:
-    header = [['Time completed', 'JobID', 'User', 'Nodes', \
-               'QueueTime', 'RunTime', 'Location', 'Proc', 'Mode']]
-else:
-    header = [['Time completed', 'JobID', 'User', 'Nodes', 'QueueTime', 'RunTime']]
+    if '-f' in sys.argv:
+        header = [['Time completed', 'JobID', 'User', 'Nodes', \
+                   'QueueTime', 'RunTime', 'Location', 'Proc', 'Mode']]
+    else:
+        header = [['Time completed', 'JobID', 'User', 'Nodes', 'QueueTime', 'RunTime']]
 
-Cobalt.Util.print_tabular([tuple(x) for x in header + uniqified])
+    Cobalt.Util.print_tabular([tuple(x) for x in header + uniqified])
