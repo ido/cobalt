aa783fbad90c836e488863c9035c2fe5371cfb7d
Narayan Desai <desai@mcs.anl.gov>
2006-09-25 19:24:33 +0000

    Remove dead code from the tester

--- src/components/testbs2.py
+++ src/components/testbs2.py
@@ -5,40 +5,6 @@ import time, sys
 
 sys.argv.append('--nodb2')
 
-def findPossible(jobs, events, tentative, depth):
-    '''find possible'''
-    print depth, 'starting findPossible', [j.get('jobid') for j in jobs], \
-          [(e.start,e.duration) for e in events], \
-          [(j.get('jobid'), p.get('name'), e) for j,p,e in tentative]
-    
-    if not jobs:
-        print depth, 'empty', [(j.get('jobid'), p.get('name'), e) for j,p,e in tentative]
-        score = evaluate(tentative)
-        return (score, tentative)
-    
-    for job,event,part in [(j,e,p) for j in jobs for e in events for p in partitions]:
-        print depth, 'checking event (', event.start, event.duration, ')'
-        for e in [event.start, event.start + event.duration]:
-            # check start of event
-            if True: #self.CanRun(job, part, e, tentative):
-                # add to tentative
-                # recurse with that job event added?
-                ten = tentative[:]
-                ten.append((job, part, e))
-                tempjobs = jobs[:]
-                tempjobs.remove(job)
-                #print 'tempjobs', [j.get('jobid') for j in tempjobs]
-                (newscore, newschedule) = findPossible(tempjobs, events + [Event(e, job.get('walltime'), 'hard', 0)], ten, depth+1)
-                if newschedule_score > best_score:
-                    best_score = newscore
-                    best_schedule = newschedule
-
-    return (best_score, best_schedule)
-                    
-    print depth, 'best was', best
-    print depth, 'tentative schedule', [(j.get('jobid'), p.get('name'), e) for j,p,e in tentative]
-    return best
-
 bs = BGSched({'configfile':'/etc/cobalt.conf', 'daemon':False})
 # jobs = JobSet()
 # partitions = PartitionSet(1, 32)
