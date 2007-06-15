07c21256880d2e3b835f7364ba4f5af89546f54d
Narayan Desai <desai@mcs.anl.gov>
2007-06-15 14:53:26 +0000

    Updates to bgctl/bridge

--- src/lib/bridge.py
+++ src/lib/bridge.py
@@ -1,9 +1,7 @@
-import sys
 import bgl_rm_api
 from ctypes import *
 
 bridge = cdll.LoadLibrary('libbglbridge.so.1')
-# saymsg = cdll.LoadLibrary('libsaymessage.so.1')
 
 bridge.rm_get_serial.restype = bgl_rm_api.status_t
 bridge.rm_set_serial.restype = bgl_rm_api.status_t
@@ -21,19 +19,14 @@ class BGStub(object):
         self.setDebug(debug)
         s = bridge.rm_set_serial(c_char_p("BGL"))
         self.attrcache = {}
-        self.settable = []
 
     def setDebug(self, level):
-#         saymsg.setSayMessageParams(saymsg.stderr, c_int(level))
         pass
 
     def fetchattr(self, attr):
-        (field, mfield, dtype, massage) = self.__attrinfo__[attr]
+        (field, dtype, massage) = self.__attrinfo__[attr]
         data = dtype()
-#         print "going to get %s (%s)" % (attr, field)
         s = bridge.rm_get_data(self.ptr, field, byref(data))
-        if s:
-            raise Exception, 'problem getting %s' % attr
         if attr not in self.nocache:
             self.attrcache[attr] = massage(data)
         return massage(data)
@@ -46,28 +39,6 @@ class BGStub(object):
             return self.attrcache[attr]
         else:
             return object.__getattribute__(self, attr)
-
-    def setattribute(self, attr, value):
-        (field, mfield, dtype, massage) = self.__attrinfo__[attr]
-        if mfield == None:
-            raise Exception, 'attr %s not settable' % attr
-        data = dtype(value)
-        s = apply(self.__modify__, (self.id, mfield, data))
-        if s:
-            raise Exception, 'problem setting %s' % attr
-#         s = bridge.rm_set_data(self.ptr, field, byref(data))
-#         if attr not in self.nocache:
-#             self.attrcache[attr] = massage(data)
-#        return massage(data)
-        if attr in self.attrcache:
-            del self.attrcache[attr]
-        return
-
-    def __setattr__(self, attr, value):
-        if attr in object.__getattribute__(self, '__attrinfo__'):
-            #try setting the attr
-            return self.setattribute(attr, value)
-        return object.__setattr__(self, attr, value)
 
     def Visible(self):
         return [item for item in self.__attrinfo__ \
@@ -99,22 +70,20 @@ class LazyRMSet(object):
         return self.data.__getitem__(num)
 
 class PreStub(BGStub):
-    __attrinfo__ = {}
     def __init__(self, pointer):
         self.attrcache = {}
         self.ptr = pointer
 
 class BasePartition(PreStub):
     __attrinfo__ = {'ID': \
-                    (bgl_rm_api.RM_BPID, None, bgl_rm_api.rm_bp_id_t, getvalue),
+                    (bgl_rm_api.RM_BPID, bgl_rm_api.rm_bp_id_t, getvalue),
                     'state': \
-                    (bgl_rm_api.RM_BPState, None, bgl_rm_api.rm_BP_state_t,
+                    (bgl_rm_api.RM_BPState, bgl_rm_api.rm_BP_state_t,
                      getvalue),
                     'location':\
-                    (bgl_rm_api.RM_BPLoc, None, bgl_rm_api.rm_location_t,
+                    (bgl_rm_api.RM_BPLoc, bgl_rm_api.rm_location_t,
                      lambda data:{'X':data.X, 'Y':data.Y, 'Z':data.Z}),
                     }
-    
     def __init__(self, pointer):
         self.attrcache = {}
         self.ptr = pointer
@@ -130,82 +99,82 @@ class Port(PreStub):
 
 class Wire(PreStub):
     __attrinfo__ = {'id': \
-                    (bgl_rm_api.RM_WireID, None, bgl_rm_api.rm_wire_id_t,
+                    (bgl_rm_api.RM_WireID, bgl_rm_api.rm_wire_id_t,
                      getvalue),
                     'state': \
-                    (bgl_rm_api.RM_WireState, None, bgl_rm_api.rm_wire_state_t,
+                    (bgl_rm_api.RM_WireState, bgl_rm_api.rm_wire_state_t,
                      getvalue),
                     'src': \
-                    (bgl_rm_api.RM_WireFromPort, None, bgl_rm_api.rm_element_t,
+                    (bgl_rm_api.RM_WireFromPort, bgl_rm_api.rm_element_t,
                      Port),
                     'dst': \
-                    (bgl_rm_api.RM_WireToPort, None, bgl_rm_api.rm_element_t,
+                    (bgl_rm_api.RM_WireToPort, bgl_rm_api.rm_element_t,
                      Port),
                     'partition': \
-                    (bgl_rm_api.RM_WirePartID, None, bgl_rm_api.pm_partition_id_t,
+                    (bgl_rm_api.RM_WirePartID, bgl_rm_api.pm_partition_id_t,
                      getvalue)
                     }
 
 class Job(PreStub):
     __attrinfo__ = \
                  {'id': \
-                  (bgl_rm_api.RM_JobID, None, bgl_rm_api.jm_job_id_t, getvalue),
+                  (bgl_rm_api.RM_JobID, bgl_rm_api.jm_job_id_t, getvalue),
                   'partition': \
-                  (bgl_rm_api.RM_JobPartitionID, None,
+                  (bgl_rm_api.RM_JobPartitionID,
                    bgl_rm_api.pm_partition_id_t, getvalue),
                   'state': \
-                  (bgl_rm_api.RM_JobState, None, bgl_rm_api.rm_job_state_t,
+                  (bgl_rm_api.RM_JobState, bgl_rm_api.rm_job_state_t,
                    bgl_rm_api.RM_JobStateEnum),
                   'executable': \
-                  (bgl_rm_api.RM_JobExecutable, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobExecutable, c_char_p, getvalue),
                   'user': \
-                  (bgl_rm_api.RM_JobUserName, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobUserName, c_char_p, getvalue),
                   'dbjobid': \
-                  (bgl_rm_api.RM_JobDBJobID, None, 
+                  (bgl_rm_api.RM_JobDBJobID,
                    bgl_rm_api.db_job_id_t, getvalue),
                   'outfile': \
-                  (bgl_rm_api.RM_JobOutFile, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobOutFile, c_char_p, getvalue),
                   'infile': \
-                  (bgl_rm_api.RM_JobInFile, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobInFile, c_char_p, getvalue),
                   'errfile': \
-                  (bgl_rm_api.RM_JobErrFile, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobErrFile, c_char_p, getvalue),
                   'outdir': \
-                  (bgl_rm_api.RM_JobOutDir, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobOutDir, c_char_p, getvalue),
                   'errtext': \
-                  (bgl_rm_api.RM_JobErrText, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobErrText, c_char_p, getvalue),
                   'args': \
-                  (bgl_rm_api.RM_JobArgs, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobArgs, c_char_p, getvalue),
                   'envs': \
-                  (bgl_rm_api.RM_JobEnvs, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobEnvs, c_char_p, getvalue),
                   'inhist': \
-                  (bgl_rm_api.RM_JobInHist, None, c_int, boolean),
+                  (bgl_rm_api.RM_JobInHist, c_int, boolean),
                   'mode': \
-                  (bgl_rm_api.RM_JobMode, None,
+                  (bgl_rm_api.RM_JobMode,
                    bgl_rm_api.rm_job_mode_t, getvalue),
                   'strace': \
-                  (bgl_rm_api.RM_JobStrace, None,
+                  (bgl_rm_api.RM_JobStrace,
                    bgl_rm_api.rm_job_strace_t, getvalue),
                   'stdin': \
-                  (bgl_rm_api.RM_JobStdinInfo, None,
+                  (bgl_rm_api.RM_JobStdinInfo,
                    bgl_rm_api.rm_job_stdin_info_t, getvalue),
                   'stdout': \
-                  (bgl_rm_api.RM_JobStdoutInfo, None,
+                  (bgl_rm_api.RM_JobStdoutInfo,
                    bgl_rm_api.rm_job_stdout_info_t, getvalue),
                   'stderr': \
-                  (bgl_rm_api.RM_JobStderrInfo, None,
+                  (bgl_rm_api.RM_JobStderrInfo,
                    bgl_rm_api.rm_job_stderr_info_t, getvalue),
                   'starttime': \
-                  (bgl_rm_api.RM_JobStartTime, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobStartTime, c_char_p, getvalue),
                   'endtime': \
-                  (bgl_rm_api.RM_JobEndTime, None, c_char_p, getvalue),
+                  (bgl_rm_api.RM_JobEndTime, c_char_p, getvalue),
                   'runtime': \
-                  (bgl_rm_api.RM_JobRunTime, None,
+                  (bgl_rm_api.RM_JobRunTime,
                    bgl_rm_api.rm_job_runtime_t, getvalue),
                   'computenodesused': \
-                  (bgl_rm_api.RM_JobComputeNodesUsed, None,
+                  (bgl_rm_api.RM_JobComputeNodesUsed,
                    bgl_rm_api.rm_job_computenodes_used_t, getvalue),
                   'exitstatus': \
-                  (bgl_rm_api.RM_JobExitStatus, None,
+                  (bgl_rm_api.RM_JobExitStatus,
                    bgl_rm_api.rm_job_exitstatus_t, getvalue),
                   }
 
@@ -217,70 +186,57 @@ class PSet(PreStub):
 
 class Partition(PreStub):
     nocache = ['Switchtail', 'BPtail', 'psetNext']
-    __modify__ = bridge.rm_modify_partition
-#     __settable__ = ['BPnum', 'Switchnum', 'NodeCardNum',
-#                     'FirstNodecard', 'NextNodecard', 'FirstBP',
-#                     'NextBP', 'FirstSwitch', 'NextSwitch', 'connection',
-#                     'user', 'mloader', 'blrts', 'linux', 'ramdisk',
-#                     'description', 'small', 'psetsPerBP']
     hidden = ['id', 'BPnum', 'BPhead', 'BPtail', 'Switchnum', 'Switchhead',
               'Switchtail']
     __attrinfo__ = \
                  {'id': \
-                  (bgl_rm_api.RM_PartitionID, None,
+                  (bgl_rm_api.RM_PartitionID,
                    bgl_rm_api.pm_partition_id_t, getvalue),
                   'state': \
-                  (bgl_rm_api.RM_PartitionState, None,
+                  (bgl_rm_api.RM_PartitionState,
                    bgl_rm_api.rm_partition_state_t,
                    bgl_rm_api.RM_PartitionStateEnum),
                   'BPnum': \
-                  (bgl_rm_api.RM_PartitionBPNum, None, c_int, getvalue),
+                  (bgl_rm_api.RM_PartitionBPNum, c_int, getvalue),
                   'BPhead': \
-                  (bgl_rm_api.RM_PartitionFirstBP, None,
-                   bgl_rm_api.rm_element_t, passthru),
+                  (bgl_rm_api.RM_PartitionFirstBP, bgl_rm_api.rm_element_t,
+                   passthru),
                   'BPtail': \
-                  (bgl_rm_api.RM_PartitionNextBP, None,
-                   bgl_rm_api.rm_element_t, passthru),
+                  (bgl_rm_api.RM_PartitionNextBP, bgl_rm_api.rm_element_t,
+                   passthru),
                   'Switchnum': \
-                  (bgl_rm_api.RM_PartitionSwitchNum, None, c_int, getvalue),
+                  (bgl_rm_api.RM_PartitionSwitchNum, c_int, getvalue),
                   'Switchhead': \
-                  (bgl_rm_api.RM_PartitionFirstSwitch, None,
+                  (bgl_rm_api.RM_PartitionFirstSwitch,
                    bgl_rm_api.rm_element_t, passthru),
                   'Switchtail': \
-                  (bgl_rm_api.RM_PartitionNextSwitch, None,
+                  (bgl_rm_api.RM_PartitionNextSwitch,
                    bgl_rm_api.rm_element_t, passthru),
                   'connection': \
-                  (bgl_rm_api.RM_PartitionConnection, None,
+                  (bgl_rm_api.RM_PartitionConnection,
                    bgl_rm_api.rm_connection_type_t,
                    bgl_rm_api.RM_ConnectionTypeEnum),
                   'user': \
-                  (bgl_rm_api.RM_PartitionUserName, bgl_rm_api.RM_MODIFY_Owner,
-                   c_char_p, getvalue),
+                  (bgl_rm_api.RM_PartitionUserName, c_char_p, getvalue),
                   'mloader': \
-                  (bgl_rm_api.RM_PartitionMloaderImg,
-                   bgl_rm_api.RM_MODIFY_MloaderImg, c_char_p, getvalue),
+                  (bgl_rm_api.RM_PartitionMloaderImg, c_char_p, getvalue),
                   'blrts': \
-                  (bgl_rm_api.RM_PartitionBlrtsImg,
-                   bgl_rm_api.RM_MODIFY_BlrtsImg, c_char_p, getvalue),
+                  (bgl_rm_api.RM_PartitionBlrtsImg, c_char_p, getvalue),
                   'linux': \
-                  (bgl_rm_api.RM_PartitionLinuxImg,
-                   bgl_rm_api.RM_MODIFY_LinuxImg, c_char_p, getvalue),
+                  (bgl_rm_api.RM_PartitionLinuxImg, c_char_p, getvalue),
                   'ramdisk': \
-                  (bgl_rm_api.RM_PartitionRamdiskImg,
-                   bgl_rm_api.RM_MODIFY_RamdiskImg, c_char_p, getvalue),
+                  (bgl_rm_api.RM_PartitionRamdiskImg, c_char_p, getvalue),
                   'mode': \
-                  (bgl_rm_api.RM_PartitionMode, None,
+                  (bgl_rm_api.RM_PartitionMode,
                    bgl_rm_api.rm_partition_mode_t,
                    bgl_rm_api.RM_PartitionModeEnum),
                   'description': \
-                  (bgl_rm_api.RM_PartitionDescription,
-                   bgl_rm_api.RM_MODIFY_Description, c_char_p, getvalue),
+                  (bgl_rm_api.RM_PartitionDescription, c_char_p, getvalue),
                   'small': \
-                  (bgl_rm_api.RM_PartitionSmall, None, c_int, boolean),
+                  (bgl_rm_api.RM_PartitionSmall, c_int, boolean),
                   'psetsPerBP': \
-                  (bgl_rm_api.RM_PartitionPsetsPerBP, None, c_int, getvalue),
+                  (bgl_rm_api.RM_PartitionPsetsPerBP, c_int, getvalue),
                   }
-    
     def __init__(self, pointer):
         PreStub.__init__(self, pointer)
         self.basePartitions = LazyRMSet(self, 'BPnum', 'BPhead',
@@ -290,28 +246,27 @@ class Partition(PreStub):
 
 class BG(BGStub):
     __attrinfo__ = {'BPsize': \
-                    (bgl_rm_api.RM_BPsize, None, bgl_rm_api.rm_size3D_t, \
+                    (bgl_rm_api.RM_BPsize, bgl_rm_api.rm_size3D_t, \
                      lambda data:{'X':data.X, 'Y':data.Y, 'Z':data.Z}),
                     'BPnum': \
-                    (bgl_rm_api.RM_BPNum, None, c_int, getvalue),
+                    (bgl_rm_api.RM_BPNum, c_int, getvalue),
                     'BPhead': \
-                    (bgl_rm_api.RM_FirstBP, None, bgl_rm_api.rm_element_t,
+                    (bgl_rm_api.RM_FirstBP, bgl_rm_api.rm_element_t,
                      passthru),
                     'BPtail': \
-                    (bgl_rm_api.RM_NextBP, None, bgl_rm_api.rm_element_t,
+                    (bgl_rm_api.RM_NextBP, bgl_rm_api.rm_element_t,
                      passthru),
                     'SwitchNum': \
-                    (bgl_rm_api.RM_SwitchNum, None, c_int, getvalue),
+                    (bgl_rm_api.RM_SwitchNum, c_int, getvalue),
                     'WireNum': \
-                    (bgl_rm_api.RM_WireNum, None, c_int, getvalue),
+                    (bgl_rm_api.RM_WireNum, c_int, getvalue),
                     'FirstWire': \
-                    (bgl_rm_api.RM_FirstWire, None, bgl_rm_api.rm_element_t,
+                    (bgl_rm_api.RM_FirstWire, bgl_rm_api.rm_element_t,
                      passthru),
                     'NextWire': \
-                    (bgl_rm_api.RM_NextWire, None, bgl_rm_api.rm_element_t,
+                    (bgl_rm_api.RM_NextWire, bgl_rm_api.rm_element_t,
                      passthru),
                     }
-    
     def __init__(self, debug=1):
         self.ptr = pointer(bgl_rm_api.rm_BGL_t())
         BGStub.__init__(self, debug)
@@ -324,14 +279,13 @@ class BG(BGStub):
 class JobList(BGStub, LazyRMSet):
     __attrinfo__ = \
                  {'size': \
-                  (bgl_rm_api.RM_JobListSize, None, c_int,
-                   lambda data:data.value),
+                  (bgl_rm_api.RM_JobListSize, c_int, lambda data:data.value),
                   'head': \
-                  (bgl_rm_api.RM_JobListFirstJob, None,
-                   bgl_rm_api.rm_element_t, lambda data:data),
+                  (bgl_rm_api.RM_JobListFirstJob, bgl_rm_api.rm_element_t, \
+                   lambda data:data),
                   'tail': \
-                  (bgl_rm_api.RM_JobListNextJob, None,
-                   bgl_rm_api.rm_element_t, lambda data:data)}
+                  (bgl_rm_api.RM_JobListNextJob, bgl_rm_api.rm_element_t, \
+                   lambda data:data)}
     nocache = ['tail']
 
     def __init__(self, flags=4095, debug=1):
@@ -343,14 +297,13 @@ class JobList(BGStub, LazyRMSet):
 class PartList(BGStub,LazyRMSet):
     __attrinfo__ = \
                  {'size': \
-                  (bgl_rm_api.RM_PartListSize, None, c_int,
-                   lambda data:data.value),
+                  (bgl_rm_api.RM_PartListSize, c_int, lambda data:data.value),
                   'head': \
-                  (bgl_rm_api.RM_PartListFirstPart, None,
-                   bgl_rm_api.rm_element_t, lambda data:data),
+                  (bgl_rm_api.RM_PartListFirstPart, bgl_rm_api.rm_element_t, \
+                   lambda data:data),
                   'tail': \
-                  (bgl_rm_api.RM_PartListNextPart, None,
-                   bgl_rm_api.rm_element_t, lambda data:data)}
+                  (bgl_rm_api.RM_PartListNextPart, bgl_rm_api.rm_element_t, \
+                   lambda data:data)}
     nocache = ['tail']
 
     def __init__(self, filter=bgl_rm_api.PARTITION_ALL_FLAG, debug=1):
@@ -363,19 +316,13 @@ if __name__ == '__main__':
     bg = BG()
     joblist = JobList(debug=4)
     partlist = PartList(debug=4)
-
     #for wire in bg.wires:
     #    print wire.src.component, wire.src.id, "=>", wire.dst.component, wire.dst.id
     #for job in joblist:
     #    print job.id, job.user, job.partition, job.state
-
     for part in partlist:
-#         print [getattr(part, name) for name in \
-#                ['id', 'description', 'small', 'connection', 'ramdisk']]
-        if part.id == 'test32_R000_J102':
-            part.description = 'modified by PyBridge'
-            part.user = 'nobody'
-            break
+        print [getattr(part, name) for name in \
+               ['id', 'description', 'small', 'connection', 'ramdisk']]
 
     for job in joblist:
         print [getattr(job, name) for name in \
