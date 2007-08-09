89a729a706dbdb7d447883c1a6fe4bf13951b004
Theron Voran <voran@mcs.anl.gov>
2007-08-09 15:28:53 +0000

    * added job start, query, and kill rpc functions (copied from bgsystem)

--- src/components/brooklyn.py
+++ src/components/brooklyn.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import logging, sys, lxml.etree, operator
+import logging, sys, lxml.etree, operator, os
 from xml.etree import ElementTree as ET
 from getopt import getopt, GetoptError
 import Cobalt.Component, Cobalt.Logging
@@ -28,6 +28,10 @@ class Brooklyn(Cobalt.Component.Component):
         self.register_function(self.GetPartition, "GetPartition")
         self.register_function(self.ReserveNodecards, "ReserveNodecards")
         self.register_function(self.ReleaseNodecards, "ReleaseNodecards")
+        self.register_function(self.StartJob, "StartJob")
+        self.register_function(self.query_jobs, "QueryJobs")
+        self.register_function(self.kill_job, "KillJob")
+
 
     def readConfigFile(self, path):
         '''reads hardware info from xml (partitions, nodecards)'''
@@ -212,6 +216,161 @@ class Brooklyn(Cobalt.Component.Component):
                 logger.info(" Empty")
             self.GenBlocked()
             return True
+
+    def StartJob(self, _, jobinfo):
+        '''starts a job'''
+        # make pipe for daemon mpirun to talk to bgsystem
+        newpipe_r, newpipe_w = os.pipe()
+
+        pid = os.fork()
+        print 'pid is', pid
+        if not pid:
+            os.close(newpipe_r)
+            os.setsid()
+            pid2 = os.fork()
+            if pid2 != 0:
+                newpipe_w = os.fdopen(newpipe_w, 'w')
+                newpipe_w.write(str(pid2))
+                newpipe_w.close()
+                os._exit(0)
+
+            #start daemonized child
+            os.close(newpipe_w)
+
+            #setup output and error files
+            if jobinfo.get('outputfile', False):
+                self.outlog = jobinfo.get('outputfile')
+            else:
+                self.outlog = tempfile.mktemp()            
+            if jobinfo.get('errorfile', False):
+                self.errlog = jobinfo.get('errorfile')
+            else:
+                self.errlog = tempfile.mktemp()
+
+            #check for location to run
+            if not jobinfo.get('location', False):
+                raise ProcessGroupCreationError, "location"
+            partition = jobinfo.get('location')[0]
+
+            #check for valid user/group
+            try:
+                userid, groupid = pwd.getpwnam(jobinfo.get('user'))[2:4]
+            except KeyError:
+                raise ProcessGroupCreationError, "user/group"
+
+            program = jobinfo.get('executable')
+            cwd = jobinfo.get('cwd')
+            pnum = str(jobinfo.get('size'))
+            mode = jobinfo.get('mode', 'co')
+            args = " ".join(jobinfo.get('args', []))
+            inputfile = jobinfo.get('inputfile', '')
+            kerneloptions = jobinfo.get('kerneloptions', '')
+            # strip out BGLMPI_MAPPING until mpirun bug is fixed 
+            mapfile = ''
+            if jobinfo.get('env', {}).has_key('BGLMPI_MAPPING'):
+                mapfile = jobinfo.get('env')['BGLMPI_MAPPING']
+                del jobinfo.get('env')['BGLMPI_MAPPING']
+            envs = " ".join(["%s=%s" % envdata for envdata in jobinfo.get('envs', {}).iteritems()])
+            atexit._atexit = []
+
+            try:
+                os.setgid(groupid)
+                os.setuid(userid)
+            except OSError:
+                logger.error("Failed to change userid/groupid for PG %s" % (jobinfo.get("pgid")))
+                sys.exit(0)
+
+            #os.system("%s > /dev/null 2>&1" % (self.config['db2_connect']))
+            os.environ["DB_PROPERTY"] = self.config['db2_properties']
+            os.environ["BRIDGE_CONFIG_FILE"] = self.config['bridge_config']
+            os.environ["MMCS_SERVER_IP"] = self.config['mmcs_server_ip']
+            os.environ["DB2INSTANCE"] = self.config['db2_instance']
+            os.environ["LD_LIBRARY_PATH"] = "/u/bgdb2cli/sqllib/lib"
+            os.environ["COBALT_JOBID"] = jobinfo.get('jobid')
+            if inputfile != '':
+                infile = open(inputfile, 'r')
+                os.dup2(infile.fileno(), sys.__stdin__.fileno())
+            else:
+                null = open('/dev/null', 'r')
+                os.dup2(null.fileno(), sys.__stdin__.fileno())
+            cmd = (self.config['mpirun'], os.path.basename(self.config['mpirun']),
+                   '-np', pnum, '-partition', partition,
+                   '-mode', mode, '-cwd', cwd, '-exe', program)
+            if args != '':
+                cmd = cmd + ('-args', args)
+            if envs != '':
+                cmd = cmd + ('-env',  envs)
+            if kerneloptions != '':
+                cmd = cmd + ('-kernel_options', kerneloptions)
+            if mapfile != '':
+                cmd = cmd + ('-mapfile', mapfile)
+
+            try:
+                err = open(self.errlog, 'a')
+                os.chmod(self.errlog, 0600)
+                os.dup2(err.fileno(), sys.__stderr__.fileno())
+            except IOError:
+                self.log.error("Job %s/%s: Failed to open stderr file %s. Stderr will be lost" % (jobinfo.get('jobid'), jobinfo.get('user'), self.errlog))
+            except OSError:
+                self.log.error("Job %s/%s: Failed to chmod or dup2 file %s. Stderr will be lost" % (jobinfo.get('jobid'), jobinfo.get('user'), self.errlog))
+
+            try:
+                out = open(self.outlog, 'a')
+                os.chmod(self.outlog, 0600)
+                os.dup2(out.fileno(), sys.__stdout__.fileno())
+            except IOError:
+                self.log.error("Job %s/%s: Failed to open stdout file %s. Stdout will be lost" % (jobinfo.get('jobid'), jobinfo.get('user'), self.outlog))
+            except OSError:
+                self.log.error("Job %s/%s: Failed to chmod or dup2 file %s. Stdout will be lost" % (jobinfo.get('jobid'), jobinfo.get('user'), self.errlog))
+
+            try:
+                apply(os.execl, cmd)
+            except Exception, e:
+                print 'got exception when trying to exec mpirun', e
+                raise SystemExit, 1
+
+            sys.exit(0)
+
+        else:
+            #parent process reads daemon child's pid through pipe
+            os.close(newpipe_w)
+            newpipe_r = os.fdopen(newpipe_r, 'r')
+            childpid = newpipe_r.read()
+            newpipe_r.close()
+            rc = os.waitpid(pid, 0)  #wait for 1st fork'ed child to quit
+            self.log.info('rc from waitpid was (%d, %d)' % rc)
+            jobinfo['pid'] = childpid
+            return jobinfo
+
+    def query_jobs(self, _, jobsinfo):
+        '''queries jobs via pid or PyBridge
+        returns those jobs that are running'''
+        return [job for job in jobsinfo
+                if self.checkpid(job.get('pid'))]
+
+    def checkpid(self, somepid):
+        '''checks if the specified pid is still around'''
+        ps = os.popen('ps ax')
+        pids = ps.readlines()
+        ps.close()
+        pidlist = [p.split()[0] for p in pids]
+        if str(somepid) in pidlist:
+            return True
+        else:
+            return False
+
+    def kill_job(self, _, jobinfo):
+        '''kills a job using via signal to pid'''
+        # status_t jm_signal_job(db_job_id_t jid, rm_signal_t signal);
+        print 'bgsystem got a kill_job call'
+        pid = jobinfo.get('pid')
+        signame = 'SIGINT'
+        try:
+            os.kill(int(pid), getattr(signal, signame))
+        except OSError, error:
+            self.log.error("Signal failure for pid %s:%s" % (pid, error.strerror))
+        return 0
+
 
 if __name__ == '__main__':
     try:
