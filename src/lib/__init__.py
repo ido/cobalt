752c0ff94d4a58f10b3528a933b34dbd7e3775f3
Narayan Desai <desai@mcs.anl.gov>
2008-02-01 15:24:37 +0000

    Make CONFIG_FILES value change based on -C (Resolves #166)

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -5,8 +5,13 @@ __revision__ = '$Revision$'
 __all__ = ['bridge', 'bgl_rm_api', 'Components', 'Data',
            'Logging', 'Proxy', 'Util']
 
+import sys
+
 # global config file specification
 # making this a tuple rather than a list makes it
 # a value, rather than a mutable object, and prevents
 # shared object-state ugliness
-CONFIG_FILES = ("/etc/cobalt.conf", )
+if '-C' in sys.argv:
+    CONFIG_FILES = (sys.argv[sys.argv.index('-C') + 1], )
+else:
+    CONFIG_FILES = ("/etc/cobalt.conf", )
