e3c13e8c275046aaca7eb57090f16612aa06ad16
Narayan Desai <desai@mcs.anl.gov>
2008-02-19 20:49:35 +0000

    Fix inclusion on posix man pages in install

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name="Cobalt",
       scripts = glob('src/clients/*.py') + glob('src/clients/POSIX/*.py') \
       + glob('src/components/*.py'),
       data_files=[('share/man/man1', glob('man/*.1')),
-                  ('share/man/man1', glob('POSIX/man/*.1')),
+                  ('share/man/man1', glob('man/POSIX/*.1')),
                   ('share/man/man5', glob('man/*.5')),
                   ('share/man/man8', glob('man/*.8'))]
       )
