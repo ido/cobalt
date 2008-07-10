86138cf88c98ccff033f4ebb64959c931dfdaf08
Daniel Buettner <buettner@alcf.anl.gov>
2008-07-10 20:20:13 +0000

    adding help info to the schedctl client

--- src/clients/schedctl.py
+++ src/clients/schedctl.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 
 import sys
+import optparse
 import Cobalt
 from Cobalt.Proxy import ComponentProxy
 
 
 if __name__ == '__main__':
+    p = optparse.OptionParser()
+    
+    p.add_option("--stop", action="store_true", dest="stop", help="stop scheduling jobs")
+    p.add_option("--start", action="store_true", dest="start", help="resume scheduling jobs")
+    p.add_option("--reread-policy", action="store_true", dest="reread", help="reread the utility function definition file")
+
+    if len(sys.argv) == 1:
+        p.print_help()
+        sys.exit(1)
+        
+    opt, args = p.parse_args()
+    
     try:
         sched = Cobalt.Proxy.ComponentProxy("scheduler")
     except ComponentLookupError:
         print >> sys.stderr, "Failed to connect to scheduler"
         sys.exit(1)
 
-    if "--stop" in sys.argv:
+    if opt.stop:
         sched.disable()
-    elif "--start" in sys.argv:
+    elif opt.start:
         sched.enable()
-    elif "--reread-policy" in sys.argv:
+    elif opt.reread:
         sched.define_user_utility_functions()
