f333cef5f44700dc4a800620b09fe8c928f34928
Narayan Desai <desai@mcs.anl.gov>
2008-07-10 15:18:19 +0000

    fix qsub

--- src/clients/POSIX/qsub.py
+++ src/clients/POSIX/qsub.py
@@ -101,8 +101,6 @@ if __name__ == '__main__':
         logger.error("Specifed mode '%s' not valid, valid modes are\n%s" % \
               (opts['mode'], "\n".join(job_types)))
         sys.exit(1)
-    if opts['mode'] == 'co' and sys_type == 'bgp':
-        opts['mode'] = 'SMP'
     if opts['mode'] == 'script':
         script_mode = os.stat(command[0])[stat.ST_MODE]
         if not (script_mode & stat.S_IXUSR or \
