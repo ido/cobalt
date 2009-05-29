56509ad00fdad8e655f9c15d016eda8590b81ca1
Narayan Desai <desai@mcs.anl.gov>
2009-05-29 14:41:35 +0000

    Version bump to 0.98.3 (final)

--- misc/cobalt.spec
+++ misc/cobalt.spec
@@ -1,6 +1,6 @@
 Summary: Cobalt System Software Suite
 Name: cobalt
-Version: 0.98.3pre6
+Version: 0.98.3
 Release: 1
 License: GPL
 Group: System Software
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Cobalt",
-      version="0.98.3pre6",
+      version="0.98.3",
       description="Cobalt Resource Manager",
       author="Cobalt Team",
       author_email="cobalt@mcs.anl.gov",
