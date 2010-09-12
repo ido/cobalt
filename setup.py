0ecd8ab7bf9bf810d36832e47f1c07a7a6b7c7a9
Paul Rich <richp@alcf.anl.gov>
2010-09-12 22:52:51 +0000

    Changes for cdbwriter component release 0.99pre15.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Cobalt",
-      version="0.99.0pre14",
+      version="0.99.0pre15",
       description="Cobalt Resource Manager",
       author="Cobalt Team",
       author_email="cobalt@mcs.anl.gov",
-      packages=["Cobalt", "Cobalt.Components", "Cobalt.DataTypes"],
+      packages=["Cobalt", "Cobalt.Components", "Cobalt.DataTypes", "Cobalt.Components.DBWriter"],
       package_dir = {'Cobalt': 'src/lib'},
       scripts = glob('src/clients/*.py') + glob('src/clients/POSIX/*.py') \
-      + glob('src/components/*.py') + glob('tools/DB_writer/cdbdump.py'),
+      + glob('src/components/*.py'),
       data_files=[('share/man/man1', glob('man/*.1')),
                   ('share/man/man1', glob('man/POSIX/*.1')),
                   ('share/man/man5', glob('man/*.5')),
