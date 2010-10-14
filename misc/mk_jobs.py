ed11e6dd31f287f099a10a059f16a11987cb0baf
Paul Rich <richp@alcf.anl.gov>
2010-10-14 22:08:21 +0000

    Improvements to the mk_jobs.py script:
      It is actually compatible with cqm again.
      It now asks for and adds projects
      It now queries dependencies
      It gets holds and sets admin versus user hold correctly.
          dep_hold, dep_fail, and maxrunning are reset based on
          the reloaded queue state.
      Formatting a bit nicer
      
      Limitation:  This should not be used with running jobs, or running jobs 
         should be purged from the output

--- misc/mk_jobs.py
+++ misc/mk_jobs.py
@@ -22,12 +22,13 @@ if __name__ == '__main__':
                       'RunTime', 'Nodes', 'State', 'Location', 'Mode', 'Procs',
                       'Queue', 'StartTime', 'Index', 'SubmitTime', 'Path',
                       'OutputDir', 'Envs', 'Command', 'Args', 'Kernel', 'KernelOptions',
-                      'Project', 'errorpath', 'outputpath', 'inputfile' ]
+                      'Project', 'errorpath', 'outputpath', 'inputfile','all_dependencies', 'user_hold',
+                      'admin_hold', 'satisfied_dependencies']
 
     query_dependencies = {'QueuedTime':['SubmitTime', 'StartTime'], 'RunTime':['StartTime']}
 
     try:
-        cqm = Cobalt.Proxy.queue_manager()
+        cqm = Cobalt.Proxy.ComponentProxy("queue-manager", defer=False)
     except Cobalt.Exceptions.ComponentLookupError:
         print "Failed to connect to queue manager"
         raise SystemExit, 1
@@ -46,7 +47,7 @@ if __name__ == '__main__':
                 for x in query_dependencies[h]:
                     if x not in header:
                         q.update({x.lower():'*'})
-    response = cqm.GetJobs(query)
+    response = cqm.get_jobs(query)
 
     def my_cmp(left, right):
         return cmp(int(left['jobid']), int(right['jobid']))
@@ -54,10 +55,11 @@ if __name__ == '__main__':
     response.sort(my_cmp)
 
     if response:
-        maxjoblen = max([len(item.get('jobid')) for item in response])
+        maxjoblen = max([len(str(item.get('jobid'))) for item in response])
         jobidfmt = "%%%ss" % maxjoblen
     # calculate derived values
     for j in response:
+        #print j
         print "cqadm.py -j %s" % j['jobid']
         command = "cqsub "
         command += "-q %s " % j['queue']
@@ -66,9 +68,9 @@ if __name__ == '__main__':
         outputpath = j.get('outputpath')
         if outputpath:
             jobname = os.path.basename(outputpath).split('.output')[0]
-            if jobname != j['jobid'].split()[0]:
+            if jobname != str(j['jobid']):
                 j['jobname'] = jobname
-        if j.has_key('jobname'):
+        if j.has_key('jobname') and (j['jobname'] != None):
             command += "-O %s " % j['jobname']
 
         envs = j.get('envs', False)
@@ -76,30 +78,46 @@ if __name__ == '__main__':
             j['envs'] = ':'.join([str(x) + '=' + str(y) for x, y in j['envs'].iteritems()])
             command += "-e %s " % j['envs']
 
-        if j.get('kernel', 'default') != "default":
+        if j.has_key('project') and (j['project'] != None):
+            command += "-p %s " % j['project']
+
+        if (j.get('kernel', 'default') != "default"):
             command += "-k %s " % j['kernel']
 
-        if j.has_key('kerneloptions'):
+        if j.has_key('kerneloptions') and (j['kerneloptions'] !=  None):
             command += "-K %s " % j['kerneloptions']
 
-        if j.has_key('errorpath'):
+        if j.has_key('errorpath') and (j['errorpath'] != None):
             command += "-E %s " % j['errorpath']
-        if j.has_key('outputpath'):
+        if j.has_key('outputpath') and (j['outputpath'] != None):
             command += "-o %s " % j['outputpath']
-        if j.has_key('inputfile'):
+        if j.has_key('inputfile') and (j['inputfile'] != None):
             command += "-i %s " % j['inputfile']
 
         command += "-t %s " % int(float(j['walltime']))
         command += "-n %s " % j['nodes']
 
-        if j['state'] == "hold":
+
+        #print j['all_dependencies']
+        #print j['satisfied_dependencies']
+
+        if j['all_dependencies'] != []:
+            command += "--dependencies %s " % ':'.join(s for s in j['all_dependencies'] if
+                                                       s not in j['satisfied_dependencies'])   
+    
+        if j['state'] == "user_hold":
             command += "-h "
 
         command += "-c %s " % j['procs']
         command += "-m %s " % j['mode']
-        command += "%s " % j['command']
-        command += "%s " % ' '.join(j['args'])
+        command += "%s" % j['command']
+        if j['args'] != []:
+            command += " %s" % ' '.join(j['args'])
 
         print "su %s -c \"%s\"" % (j['user'], command)
+
+        if j['admin_hold']:
+            print "cqadm --hold %s" % j['jobid']
+
 
 
