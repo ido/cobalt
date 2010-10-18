b504ee977d01ee991b7ecd92d9c2df5e36a88599
Paul Rich <richp@alcf.anl.gov>
2010-10-18 17:32:26 +0000

    mk_queues updated to reflect what is in use on Intrepid.

--- misc/mk_queues.py
+++ misc/mk_queues.py
@@ -5,53 +5,58 @@ __revision__ = '$Revision: 619 $'
 __version__ = '$Version$'
 
 import sys, xmlrpclib
-import Cobalt.Logging, Cobalt.Proxy, Cobalt.Util
+import Cobalt.Logging, Cobalt.Util
 import getpass
+from Cobalt.Proxy import ComponentProxy
+from Cobalt.Exceptions import ComponentLookupError
+
 
 def get_queues(cqm_conn):
     '''gets queues from cqmConn'''
     info = [{'tag':'queue', 'name':'*', 'state':'*', 'users':'*',
              'maxtime':'*', 'mintime':'*', 'maxuserjobs':'*',
              'maxusernodes':'*', 'maxqueued':'*', 'maxrunning':'*',
-             'adminemail':'*', 'totalnodes':'*', 'cron':'*', 'policy':'*'}]
-    return cqm_conn.GetQueues(info)
+             'adminemail':'*', 'totalnodes':'*', 'cron':'*', 'policy':'*', 'priority':'*'}]
+    return cqm_conn.get_queues(info)
 
 if __name__ == '__main__':
     if '--version' in sys.argv:
-        print "cqadm.py %s" % __revision__
+        print "cqadm %s" % __revision__
         print "cobalt %s" % __version__
         raise SystemExit, 0
 
     try:
-        cqm = Cobalt.Proxy.queue_manager()
-    except Cobalt.Exceptions.ComponentLookupError:
-        print "Failed to connect to queue manager"
-        raise SystemExit, 1
+        cqm = ComponentProxy("queue-manager", defer=False)
+    except ComponentLookupError:
+        print >> sys.stderr, "Failed to connect to queue manager"
+        sys.exit(1)
 
     response = get_queues(cqm)
 
     for q in response:
         name = q.get('name')
-        print "cqadm.py --addq %s" % name
-        if q.get('users', '*') != '*':
-            print "cqadm.py --setq users=%s %s" % (q.get('users'), name)
-        if q.get('mintime', '*') != '*':
-            print "cqadm.py --setq mintime=%s %s" % (q.get('mintime'), name)
-        if q.get('maxtime', '*') != '*':
-            print "cqadm.py --setq maxtime=%s %s" % (q.get('maxtime'), name)
-        if q.get('maxrunning', '*') != '*':
-            print "cqadm.py --setq maxrunning=%s %s" % (q.get('maxrunning'), name)
-        if q.get('maxqueued', '*') != '*':
-            print "cqadm.py --setq maxqueued=%s %s" % (q.get('maxqueued'), name)
-        if q.get('maxusernodes', '*') != '*':
-            print "cqadm.py --setq maxusernodes=%s %s" % (q.get('maxusernodes'), name)
-        if q.get('totalnodes', '*') != '*':
-            print "cqadm.py --setq totalnodes=%s %s" % (q.get('totalnodes'), name)
-        if q.get('adminemail', '*') != '*':
-            print "cqadm.py --setq adminemail=%s %s" % (q.get('adminemail'), name)
-        if q.get('state', '*') != '*':
-            print "cqadm.py --setq state=%s %s" % (q.get('state'), name)
-        if q.get('cron', '*') != '*':
-            print "cqadm.py --setq cron=%s %s" % (q.get('cron'), name)
-        if q.get('policy', '*') != '*':
-            print "cqadm.py --setq policy=%s %s" % (q.get('policy'), name)
+        print "cqadm --addq %s" % name
+        if q.get('users'):
+            print "cqadm --setq users=%s %s" % (q.get('users'), name)
+        if q.get('mintime'):
+            print "cqadm --setq mintime=%s %s" % (q.get('mintime'), name)
+        if q.get('maxtime'):
+            print "cqadm --setq maxtime=%s %s" % (q.get('maxtime'), name)
+        if q.get('maxrunning'):
+            print "cqadm --setq maxrunning=%s %s" % (q.get('maxrunning'), name)
+        if q.get('maxqueued'):
+            print "cqadm --setq maxqueued=%s %s" % (q.get('maxqueued'), name)
+        if q.get('maxusernodes'):
+            print "cqadm --setq maxusernodes=%s %s" % (q.get('maxusernodes'), name)
+        if q.get('totalnodes'):
+            print "cqadm --setq totalnodes=%s %s" % (q.get('totalnodes'), name)
+        if q.get('adminemail'):
+            print "cqadm --setq adminemail=%s %s" % (q.get('adminemail'), name)
+        if q.get('state'):
+            print "cqadm --setq state=%s %s" % (q.get('state'), name)
+        if q.get('cron'):
+            print "cqadm --setq cron=%s %s" % (q.get('cron'), name)
+        if q.get('policy'):
+            print "cqadm --setq policy=%s %s" % (q.get('policy'), name)
+        if q.get('priority'):
+            print "cqadm --setq priority=%s %s" % (q.get('priority'), name)
