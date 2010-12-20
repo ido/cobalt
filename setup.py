bb8ab2261e812f12a01a182fb43af465e9f024cc
Paul Rich <richp@alcf.anl.gov>
2010-12-20 06:03:12 +0000

    Setting version numbers for 0.99.0pre22 release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Cobalt",
-      version="0.99.0pre21",
+      version="0.99.0pre22",
       description="Cobalt Resource Manager",
       author="Cobalt Team",
       author_email="cobalt@mcs.anl.gov",
