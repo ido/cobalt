ac18f134658ba1c2f141e14811df89b0d44e68de
Paul Rich <richp@alcf.anl.gov>
2011-01-18 22:42:26 +0000

    showres now can show timezones for localtime display.  disable with --oldts

--- src/clients/showres.py
+++ src/clients/showres.py
@@ -5,10 +5,13 @@ __version__ = '$Version$'
 
 import sys, time
 import math
+import optparse
 import Cobalt.Logging, Cobalt.Util
 from Cobalt.Proxy import ComponentProxy
 from Cobalt.Exceptions import ComponentLookupError
 
+
+__helpmsg__ = "Usage: showres [-l] [--oldts] [--version]"
 
 def mergelist(location_string, cluster):
     if not cluster:
@@ -17,6 +20,15 @@ def mergelist(location_string, cluster):
     locations = location_string.split(":")
 
     return Cobalt.Util.merge_nodelist(locations)
+
+#def parse_options()
+#    p = optparse.OptionParser()
+#    p.add_option("-l", "--long", action="store_true", default=False
+#            dest="long", help="shows long display")
+#    p.add_option(None, "--version" action="store_true", default=False,
+#            dest="version", help="Prints version information")
+#    p.add_option(None, 
+
 
 if __name__ == '__main__':
     if '--version' in sys.argv:
@@ -41,28 +53,38 @@ if __name__ == '__main__':
         'start':'*', 'duration':'*', 'partitions':'*', 'cycle': '*', 
         'queue': '*', 'res_id': '*', 'cycle_id': '*'}])
     output = []
+ 
+    time_fmt = "%c"
+    if not ('--oldts' in sys.argv):
+        time_fmt += " %Z"
+    
     if '-l' in sys.argv:
         verbose = True
-        header = [('Reservation', 'Queue', 'User', 'Start', 'Duration', 'End Time', 'Cycle Time', 'Partitions')]
+        header = [('Reservation', 'Queue', 'User', 'Start', 'Duration', 
+            'End Time', 'Cycle Time', 'Partitions')]
     else:
         verbose = False
-        header = [('Reservation', 'Queue', 'User', 'Start', 'Duration', 'Partitions')]
+        header = [('Reservation', 'Queue', 'User', 'Start', 'Duration', 
+            'Partitions')]
 
     for res in reservations:
         start = float(res['start'])
         duration = float(res['duration'])
-        # do some crazy stuff to make reservations which cycle display the "next" start time
+        # do some crazy stuff to make reservations which cycle display the 
+        # "next" start time
         if res['cycle']:
             cycle = float(res['cycle'])
             now = time.time()
             periods = math.floor((now - start)/cycle)
-            # reservations can't become active until they pass the start time -- so negative periods aren't allowed
+            # reservations can't become active until they pass the start time 
+            # -- so negative periods aren't allowed
             if periods < 0:
                 pass
             # if we are still inside the reservation, show when it started
             elif (now - start) % cycle < duration:
                 start += periods * cycle
-            # if we are in the dead time after the reservation ended, show when the next one starts
+            # if we are in the dead time after the reservation ended, show 
+            # when the next one starts
             else:
                 start += (periods+1) * cycle
         if res['cycle_id'] == None:
@@ -79,12 +101,18 @@ if __name__ == '__main__':
         dmin = (duration/60)%60
         dhour = duration/3600
         if verbose:
-            output.append((res['name'], res['queue'], res['users'], time.strftime("%c", time.localtime(start)),
-                           "%02d:%02d" % (dhour, dmin),time.strftime("%c", time.localtime(start + duration)), cycle, mergelist(res['partitions'], cluster)))
+            output.append((res['name'], res['queue'], res['users'], 
+                time.strftime(time_fmt, time.localtime(start)),
+                "%02d:%02d" % (dhour, dmin),
+                time.strftime(time_fmt, time.localtime(start + duration)), cycle, 
+                mergelist(res['partitions'], cluster)))
         else:
-            output.append((res['name'], res['queue'], res['users'], time.strftime("%c", time.localtime(start)),
-                           "%02d:%02d" % (dhour, dmin), mergelist(res['partitions'], cluster)))
+            output.append((res['name'], res['queue'], res['users'], 
+                time.strftime(time_fmt, time.localtime(start)),
+                "%02d:%02d" % (dhour, dmin), 
+                mergelist(res['partitions'], cluster)))
 
-    output.sort( (lambda x,y: cmp( time.mktime(time.strptime(x[3], "%c")), time.mktime(time.strptime(y[3], "%c"))) ) )
+    output.sort( (lambda x,y: cmp( time.mktime(time.strptime(x[3], "%c")), 
+        time.mktime(time.strptime(y[3], "%c"))) ) )
     Cobalt.Util.print_tabular(header + output)
                      
