5ca8b5a0ee6e08f97732cca3173e0db8b6cac298
Paul Rich <richp@alcf.anl.gov>
2011-05-27 15:59:29 +0000

    Cobalt-mpirun modified to exec mpirun directly after setting GID to user.
    It no longer consults cobalt when it tries to launch a job.

--- src/clients/cobalt-mpirun.py
+++ src/clients/cobalt-mpirun.py
@@ -1,18 +1,64 @@
 #!/usr/bin/env python
 
-'''Cobalt mpirun'''
+'''Cobalt mpirun
+
+This mediates the execution of an mpirun on behalf of a user running a script
+job.  When invoked, the script expects the following environment variables:
+
+    COBALT_NODEFILE
+    COBALT_JOBID
+    COBALT_RESID
+    COBALT_PAETNAME
+    COBALT_JOBSIZE
+
+Once invoked, this will handle argument mapping to mpirun.
+
+
+'''
 __revision__ = ''
 __version__ = '$Version$'
 
-import getopt, os, pwd, sys, time, xmlrpclib, logging
-import Cobalt.Logging, Cobalt.Util
+import getopt
+import os 
+import pwd
+import sys
+import time
+import xmlrpclib
+import logging
+import ConfigParser
+
+import Cobalt.Logging
 from Cobalt.Proxy import ComponentProxy
 from Cobalt.Exceptions import ComponentLookupError
+from Cobalt import CONFIG_FILES
 import Cobalt.Util
 
 usehelp = "Usage:\ncobalt-mpirun [--version] [-h] <mpirun arguments>"
 
+config = ConfigParser.ConfigParser()
+config.read(CONFIG_FILES)
+
+def get_config_entry(section, option, default=None):
+
+    if not config.has_section(section):
+        return default
+    try:
+        value = config.get(section,option)
+    except ConfigParser.NoOptionError:
+        value = default
+    return value
+
+def check_env(env_name):
+    try:
+        os.environ[env_name]
+    except KeyError:
+        print "Environment variable %s expected, but not found! Aborting." % env_name
+        sys.exit(1)
+
+
 if __name__ == '__main__':
+
+    #RUNNING AS COBALT GROUP:
     if '--version' in sys.argv:
         print "cobalt-mpirun %s" % __revision__
         print "cobalt %s" % __version__
@@ -28,7 +74,7 @@ if __name__ == '__main__':
         to run your job.
         """
         
-        raise SystemExit, 0
+        raise SystemExit, 1
     try:
         idx = sys.argv.index("-partition")
         arglist = sys.argv[1:idx] + sys.argv[idx+2:]
@@ -36,6 +82,39 @@ if __name__ == '__main__':
         print "will run in the partition reserved by cobalt."
     except ValueError:
         arglist = sys.argv[1:]
+
+
+    #NOTE: validation of jobid, resid, and partition information is being handled
+    #by the scheduler library that mpirun includes.  I'm not sure if we should
+    #do some sort of attempted internal validation --PMR
+    expected_envs = ["COBALT_JOBID", "COBALT_PARTNAME", "COBALT_PARTSIZE"]
+
+    run_cmd = get_config_entry("bgpm", "mpirun", None)
+    if run_cmd == None:
+        print "FATAL: cobalt.conf entry for mpirun not found.  Aborting run."
+        sys.exit(1)
+
+    for expected_env in expected_envs:
+        #if this check fails, the program will abort
+        check_env(expected_env)
+
+    #setgid as user:
+    try:
+        user = pwd.getpwuid(os.getuid()).pw_name
+        print user
+    except:
+        print "FATAL: failed to find a legitimate uid."
+        sys.exit(1)
+    try:        
+        os.setgid(pwd.getpwnam(user).pw_gid)
+    except OSError:
+        print "FATAL: failed to reset group" 
+        sys.exit(1)
+
+
+
+    #BEYOND THIS POINT, RUNNING AS USER'S GID
+
 
     # these flags (which all take an argument) should not be passed to the real mpirun
     bad_args = ["-host", "-backend", "-shape"]
@@ -71,38 +150,37 @@ if __name__ == '__main__':
         logger.error("cobalt-mpirun must be invoked by a script submitted to cobalt.")
         raise SystemExit, 1
 
-    try:
-        cqm = ComponentProxy("queue-manager", defer=False)
-    except ComponentLookupError:
-        print >> sys.stderr, "Failed to connect to queue manager"
-        sys.exit(1)
-        
-    response = cqm.get_jobs([{'tag':'job', 'jobid':int(os.environ["COBALT_JOBID"]), 'state':'*', 'procs':'*', 'location':'*', 'walltime':'*', 'outputdir':'*'}])
-    if len(response) == 0:
-        logger.error("Error: cqm did not find a job with id " + os.environ["COBALT_JOBID"])
-        raise SystemExit, 1
-    if len(response) > 1:
-        logger.error("Error: cqm did not find a unique job with id " + os.environ["COBALT_JOBID"])
-        raise SystemExit, 1
-         
-    j = response[0]
-    if j['location'] is None:
-        logger.error("Error: cobalt-mpirun's parent is in state '%s' and has not specified a partition." % j['state'])
-        raise SystemExit, 1
-#    j['location'] = "ANLR00"
-    
-    arglist = ['-partition', j['location'][0]] + arglist
+    arglist = ['-partition', os.environ["COBALT_PARTNAME"]] + arglist
 
     # update the current working directory, if not specified on the command line
     # however, mpirun -free gets angry if you specify -cwd, so check for that    
     if "-cwd" not in arglist and "-free" not in arglist:
         arglist = ['-cwd', os.getcwd()] + arglist
 
+
     # Add cobalt jobid environment variable to script job, but again, not to be used
     # along with mpirun -free
+    env_str = ''
+    if '-env' in arglist:
+        env_str = arglist[sys.argv.index("-env")+1]
+        arglist.remove(env_str)
+        arglist.remove("-env")
+
+
+    cobalt_env_str = "COBALT_JOBID=%s" % os.getenv("COBALT_JOBID")
+    if os.environ.has_key("COBALT_RESID"):
+        cobalt_env_str = cobalt_env_str + ":COBALT_RESID=%s" % os.environ["COBALT_RESID"]
+    if os.environ.has_key("COBALT_JOB_ENVS"):
+        cobalt_env_str = cobalt_env_str + ":" + os.environ("COBALT_JOB_ENVS")
+
+    env_str = env_str + cobalt_env_str
+
     if "-free" not in arglist:
         arglist = ['-env', 'COBALT_JOBID='+os.environ["COBALT_JOBID"]] + arglist
     
+    if env_str != '':
+        arglist = ['-env', env_str] + arglist
+
     if "-np" in sys.argv:
         idx = sys.argv.index("-np")
     elif "-n" in sys.argv:
@@ -113,70 +191,22 @@ if __name__ == '__main__':
         idx = -1
      
     if idx > 0:
-        if int(sys.argv[idx+1]) > (int(j['procs']) * 4):
-            logger.error("Error: tried to request more processors (%s) than reserved (%s)." % (sys.argv[idx+1], j['procs']))
+        if int(sys.argv[idx+1]) > (int(os.environ["COBALT_PARTSIZE"]) * 4):
+            logger.error("Error: tried to request more processors (%s) than reserved (%s)." % \
+                    (sys.argv[idx+1], os.environ["COBALT_PARTSIZE"]))
             raise SystemExit, 1
         
-    user = pwd.getpwuid(os.getuid())[0]
-
-    jobspec = {'jobid':int(os.environ["COBALT_JOBID"]), 'user':user, 'true_mpi_args':arglist, 
-               'walltime':j['walltime'], 'args':[], 'location':j['location'], 'outputdir':j['outputdir'], }
     for key in io_redirect:
         if io_redirect[key]:
             jobspec.update({key: io_redirect[key]})
-               
+
+    #don't fork, just exec. Keep the PID the same for user scripts (?)
+    arglist.insert(0, run_cmd)
     try:
-        scriptm = ComponentProxy("script-manager", defer=False)
-        system = ComponentProxy("system", defer=False)
-
-        # try adding job to queue_manager
-        pgid = int(scriptm.invoke_mpi_from_script(jobspec))
-        
-        # give the process a chance to get started before we check for it
-        start = time.time()
-        while True:
-            r = system.get_process_groups([{'id':pgid, 'state':'*'}])
-            if r:
-                break
-            
-            # we'll give it 90 seconds to get started
-            if time.time() - start > 90:
-                break
-            
-            Cobalt.Util.sleep(5)
-        
-        while True:
-            r = system.get_process_groups([{'id':pgid, 'state':'*'}])
-            if r and r[0]['state'] == 'running':
-                Cobalt.Util.sleep(5)
-                continue
-            else:
-                break
-        print "process group %s has completed" % (pgid)
-        result = system.wait_process_groups([{'id':pgid, 'exit_status':'*'}])
-        
-        raise SystemExit, result[0].get('exit_status')
-        
-
-    except ComponentLookupError:
-        logger.error("Trouble communicating with Cobalt components")
-        raise SystemExit, 1
-#    except xmlrpclib.Fault, flt:
-#        if flt.faultCode == 31:
-#            logger.error("System draining. Try again later")
-#            raise SystemExit, 1
-#        elif flt.faultCode == 30:
-#            logger.error("Job submission failed because: \n%s\nCheck 'cqstat -q' and the cqstat manpage for more details." % flt.faultString)
-#            raise SystemExit, 1
-#        elif flt.faultCode == 1:
-#            logger.error("Job submission failed due to queue-manager failure")
-#            raise SystemExit, 1
-#        else:
-#            logger.error("Job submission failed")
-#            logger.error(flt)
-#            raise SystemExit, 1
-#     except:
-#         logger.error("Error submitting job")
-#         raise SystemExit, 1
+        os.execvpe(run_cmd, arglist, os.environ)
+    except Exception, e:
+        print >> sys.stderr, "error executing %s" % opt.executable
+        print >> sys.stderr, e
+        sys.exit(1)
 
 
