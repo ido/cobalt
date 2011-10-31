7fd5dda7b67510f8c66cc4bcb745e0516bee87e9
Brian Toonen <toonen@alcf.anl.gov>
2011-10-31 22:17:41 +0000

    bump to version 0.99.0pre35

--- misc/cobalt.spec
+++ misc/cobalt.spec
@@ -1,6 +1,6 @@
 Summary: Cobalt System Software Suite
 Name: cobalt
-Version: 0.99.0pre34
+Version: 0.99.0pre35
 
 Release: 1
 License: GPL
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Cobalt",
-      version="0.99.0pre34",
+      version="0.99.0pre35",
       description="Cobalt Resource Manager",
       author="Cobalt Team",
       author_email="cobalt@mcs.anl.gov",
