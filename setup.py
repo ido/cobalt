af32a2baf439fe817edf3c16682b4a6b26584e7e
Paul Rich <richp@alcf.anl.gov>
2012-02-17 19:34:25 +0000

    updating setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from glob import glob
 
 setup(name="Cobalt",
-      version="0.99.1beta.1",
+      version="0.99.1beta.2",
       description="Cobalt Resource Manager",
       author="Cobalt Team",
       author_email="cobalt@mcs.anl.gov",
