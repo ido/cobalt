c200ea89779405ca1edb6f3d4155271b96b5c403
Paul <richp@alcf.anl.gov>
2015-08-21 21:59:07 +0000

    Fixing timezone issues with tests.  Run in UTC.

--- testsuite/TestCobaltClients/test_files/slpstat_test.py
+++ testsuite/TestCobaltClients/test_files/slpstat_test.py
@@ -12,11 +12,11 @@ def test_slpstat_arg_1():
     cmdout    = \
 """Name  Location  Update Time               
 ==========================================
-S0    P0        Mon Apr 22 17:06:10 2013  
-S1    P1        Mon Apr 22 17:06:20 2013  
-S2    P2        Mon Apr 22 17:06:30 2013  
-S3    P3        Mon Apr 22 17:06:40 2013  
-S4    P4        Mon Apr 22 17:06:50 2013  
+S0    P0        Mon Apr 22 22:06:10 2013  
+S1    P1        Mon Apr 22 22:06:20 2013  
+S2    P2        Mon Apr 22 22:06:30 2013  
+S3    P3        Mon Apr 22 22:06:40 2013  
+S4    P4        Mon Apr 22 22:06:50 2013  
 """
 
     cmderr    = ''
@@ -116,11 +116,11 @@ def test_slpstat_arg_3():
     cmdout    = \
 """Name  Location  Update Time               
 ==========================================
-S0    P0        Mon Apr 22 17:06:10 2013  
-S1    P1        Mon Apr 22 17:06:20 2013  
-S2    P2        Mon Apr 22 17:06:30 2013  
-S3    P3        Mon Apr 22 17:06:40 2013  
-S4    P4        Mon Apr 22 17:06:50 2013  
+S0    P0        Mon Apr 22 22:06:10 2013  
+S1    P1        Mon Apr 22 22:06:20 2013  
+S2    P2        Mon Apr 22 22:06:30 2013  
+S3    P3        Mon Apr 22 22:06:40 2013  
+S4    P4        Mon Apr 22 22:06:50 2013  
 """
 
     cmderr    = \
@@ -173,11 +173,11 @@ def test_slpstat_debug_1():
     cmdout    = \
 """Name  Location  Update Time               
 ==========================================
-S0    P0        Mon Apr 22 17:06:10 2013  
-S1    P1        Mon Apr 22 17:06:20 2013  
-S2    P2        Mon Apr 22 17:06:30 2013  
-S3    P3        Mon Apr 22 17:06:40 2013  
-S4    P4        Mon Apr 22 17:06:50 2013  
+S0    P0        Mon Apr 22 22:06:10 2013  
+S1    P1        Mon Apr 22 22:06:20 2013  
+S2    P2        Mon Apr 22 22:06:30 2013  
+S3    P3        Mon Apr 22 22:06:40 2013  
+S4    P4        Mon Apr 22 22:06:50 2013  
 """
 
     cmderr    = \
