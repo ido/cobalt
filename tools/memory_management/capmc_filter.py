4f6fe4304d6ed238fc183dbc5b033492a5e02be3
Paul Rich <richp@alcf.anl.gov>
2017-04-14 16:25:38 -0500

    can now update attrs from filter and filter now will coerce memory modes for preset queues.

--- tools/memory_management/capmc_filter.py
+++ tools/memory_management/capmc_filter.py
@@ -2,11 +2,20 @@
 
 import sys
 import ast
-AVAILABLE_MCDRAM = ['cache', 'hybrid', 'half', 'flat']
+AVAILABLE_MCDRAM = ['cache', 'equal', 'split', 'flat']
 AVAILABLE_NUMA = ['a2a', 'snc2', 'snc4', 'hemi', 'quad']
+
+PRESET_QUEUES = {'debug-flat-quad': ('flat', 'quad'),
+                 'debug-cache-quad': ('cache', 'quad'),
+                }
+
 
 def main():
 
+    attrs = {}
+    mcdram = None
+    numa = None
+    queue = None
     for arg in sys.argv:
         try:
             splitarg = arg.split('=')
@@ -28,8 +37,20 @@ def main():
                         return 1
                 else:
                     pass
+            attrs = attr_dict
+        elif key == 'queue':
+            if val in PRESET_QUEUES.keys():
+                mcdram = PRESET_QUEUES[val][0]
+                numa = PRESET_QUEUES[val][1]
+                queue = val
         else:
             pass
+
+    if mcdram is not None and numa is not None:
+        attrs['mcdram'] = mcdram
+        attrs['numa'] = numa
+        print >> sys.stderr, "Memory mode set to %s %s for queue %s" % (mcdram, numa, queue)
+        print "attrs=%s" % attrs
 
     return 0
 
