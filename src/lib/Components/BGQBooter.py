975f8fa76d3bab34b70ad43699c6ac75cb55516d
Eric Roy Pershey <pershey@alcf.anl.gov>
2018-11-27 22:48:58 +0000

    Update BGQBooter.py

--- src/lib/Components/BGQBooter.py
+++ src/lib/Components/BGQBooter.py
@@ -1,6 +1,6 @@
-'''Booting Thread for Cobalt's BGQ system component.
 # Copyright 2017 UChicago Argonne, LLC. All rights reserved.
 # Licensed under a modified BSD 3-clause license. See LICENSE for details.
+'''Booting Thread for Cobalt's BGQ system component.
 
 '''
 import logging
