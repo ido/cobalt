3a6c9ac73fc6464ae9201e39548e18053fef454c
Eric Roy Pershey <pershey@alcf.anl.gov>
2018-11-27 22:52:26 +0000

    Update cdbMessages.py

--- src/lib/Components/DBWriter/cdbMessages.py
+++ src/lib/Components/DBWriter/cdbMessages.py
@@ -1,6 +1,6 @@
-import datetime
 # Copyright 2017 UChicago Argonne, LLC. All rights reserved.
 # Licensed under a modified BSD 3-clause license. See LICENSE for details.
+import datetime
 try:
     import json
 except ImportError:
