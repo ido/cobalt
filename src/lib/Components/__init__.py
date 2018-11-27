5d92ce6863d50d83a5e08baec001a15458479690
Eric Roy Pershey <pershey@alcf.anl.gov>
2018-11-27 22:53:39 +0000

    Update __init__.py

--- src/lib/Components/__init__.py
+++ src/lib/Components/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["system", "DBWriter"]
 # Copyright 2017 UChicago Argonne, LLC. All rights reserved.
 # Licensed under a modified BSD 3-clause license. See LICENSE for details.
+__all__ = ["system", "DBWriter"]
 
